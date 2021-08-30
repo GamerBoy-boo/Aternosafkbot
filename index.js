5bf43362d368c2da6207d35d272220005e25ae75
GamerBoy-boo <79464457+GamerBoy-boo@users.noreply.github.com>
2021-08-30 21:08:52 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ let data = JSON.parse(rawdata);
 var lasttime = -1;
 var moving = 0;
 var connected = 0;
-var actions = [ 'forward', 'back', 'left', 'right']
+var actions = [ 'forward', 'back', 'left', 'right', 'jump']
 var lastaction;
 var pi = 3.14159;
 var moveinterval = 2; // 2 second movement interval
-var maxrandom = 5; // 0-5 seconds added to movement interval (randomly)
+var maxrandom = 3; // 0-5 seconds added to movement interval (randomly)
 var host = data["ip"];
 var username = data["name"]
 var nightskip = data["auto-night-skip"]
@@ -28,8 +28,10 @@ bot.loadPlugin(cmd)
 
 
 bot.on('login',function(){
-	console.log("Logged In")
-	bot.chat("/login gamer321");
+	console.log("Logged In,Developed By Huzaifa")
+	bot.chat("/login gamer321")
+  bot.chat("/minecraft:tp Gamer321123 -102.08 60.00 -85.89")
+  bot.chat("/minecraft:tp Gamer321123 -102.08 60.00 -85.89");
 });
 
 bot.on('time', function(time) {
